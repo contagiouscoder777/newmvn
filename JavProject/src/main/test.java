c63cb68a5e9b1b9d127dfb1d1055647454443021
TechSmith90210 <sshaikhdev@gmail.com>
2023-11-07 23:43:46 +0530

    first commit

---
+++ JavProject/src/main/test.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\salma\\Downloads\\chromedriver_win32");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.example.com");
+        driver.quit();
+    }
+}
